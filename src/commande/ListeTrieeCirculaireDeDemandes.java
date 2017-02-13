8cee10389f2ebd65d7144515d7822013c0246db6
alexisbrunet <06alexisbrunet@gmail.com>
2017-02-13 10:16:19 +0100

    ajout ListeTrieeCirculaireDeDemandes
    to String non fonctionnelle
    manque javadoc

---
+++ src/commande/ListeTrieeCirculaireDeDemandes.java
@@ -0,0 +1,127 @@
+package commande;
+
+import java.util.ArrayList;
+
+import outils.Demande;
+import outils.Sens;
+
+public class ListeTrieeCirculaireDeDemandes implements IListeTrieeCirculaire{
+
+	private ArrayList<Demande> listeTrieeCirculaireDeDemandes;
+	
+	public ListeTrieeCirculaireDeDemandes(int length)
+	{
+		listeTrieeCirculaireDeDemandes = new ArrayList<Demande>(length);
+	}
+	
+	@Override
+	public int taille() {
+		return listeTrieeCirculaireDeDemandes.size();
+	}
+
+	@Override
+	public boolean estVide() {
+		return listeTrieeCirculaireDeDemandes.isEmpty();
+	}
+
+	@Override
+	public void vider() {
+		listeTrieeCirculaireDeDemandes.clear();
+	}
+
+	@Override
+	public boolean contient(Object e) {
+		return listeTrieeCirculaireDeDemandes.contains(e);
+	}
+
+	@Override
+	public void inserer(Object e) {
+		listeTrieeCirculaireDeDemandes.add((Demande) e);
+	}
+
+	@Override
+	public void supprimer(Object e) {
+		listeTrieeCirculaireDeDemandes.remove(e);
+		
+	}
+
+	@Override
+	public Object suivantDe(Object courant) {
+		Demande demandeSuivanteTriee = new Demande();
+		for (Demande demande : this.listeTrieeCirculaireDeDemandes) {
+			if(demande.sens().equals(((Demande) courant).sens()) && 
+					((Demande) courant).etage() < demandeSuivanteTriee.etage() &&
+					demande.etage() > demandeSuivanteTriee.etage())
+			{
+				demande = demandeSuivanteTriee;
+			}
+		}
+		return demandeSuivanteTriee;
+	}
+	
+	@Override
+	public String toString() {
+		//permet de construire la String to return
+		String s ="";
+		// la liste triee apres jointure entre la montée et la descente
+		ArrayList<Demande> listeTriee = new ArrayList<Demande>();
+		// permet de stocker toutes les montées
+		ArrayList<Demande> listeMontee = new ArrayList<Demande>();
+		// permet de stocker toutes les descentes
+		ArrayList<Demande> listeDescente = new ArrayList<Demande>();
+		// on itere sur la donnée membre listeTrieeCirculaireDeDemandes où on a stockée toutes les demandes
+		for (Demande demande : listeTrieeCirculaireDeDemandes) 
+		{
+			// gestion de la montée
+			if(demande.sens() == Sens.MONTEE)
+			{
+				// si la liste montée et vide ou si le dernier element de la iste montée est plus petit que l'element couranr de la boucle
+				if(listeMontee.isEmpty() || listeMontee.get(listeMontee.size()).etage() < demande.etage())
+				{
+					// on ajoute
+					listeMontee.add(demande);
+				}
+				// si le dernier element de la iste montée est plus grand que l'element couranr de la boucle
+				else
+				{
+					// on recupere la position du dernier element
+					Demande demandeToMove = listeMontee.get(listeMontee.size());
+					// on switch les positions entre l'element à insérer et le dernier element plus grand
+					listeMontee.add(listeMontee.size(), demande);
+					listeMontee.add(demandeToMove);
+				}
+			}
+			else if(demande.sens() == Sens.DESCENTE)
+			{
+				if(listeDescente.isEmpty() || listeDescente.get(listeDescente.size()).etage() > demande.etage())
+				{
+					listeDescente.add(demande);
+				}
+				else
+				{
+					Demande demandeToMove = listeDescente.get(listeDescente.size());
+					listeDescente.add(listeDescente.size(), demande);
+					listeDescente.add(demandeToMove);
+				}
+			}
+		}
+		//jointure
+		listeMontee.addAll(listeDescente);
+		listeTriee = listeMontee;
+		// composition de la string to return easy
+		s+="[ ";
+		for (int i = 0; i < listeTriee.size();i++) {
+			if(i == listeTriee.size())
+			{
+				s+=listeTriee.get(i).toString();
+			}
+			else
+			{
+				s+=listeTriee.get(i).toString() +", ";
+			}
+		}
+		s+=" ]";
+		return s;
+	}
+
+}
