0c943c5e41069ef815549b47c4d29402c883ee00
alexisbrunet <06alexisbrunet@gmail.com>
2017-03-07 13:34:24 +0100

    ajout icabine et doublure de cabine

--- src/cabine/ICabine.java
+++ src/cabine/ICabine.java
@@ -12,4 +12,26 @@ public interface ICabine
 	 */
 	void signalerChangementDEtage();
 	
+	/** 
+	 * Permet de modifier le sens de deplacement de la cabine vers la MONTEE.
+	 */
+	void monter();
+	
+	/** 
+	 * Permet de modifier le sens de deplacement de la cabine vers la DESCENTE.
+	 */
+	void descendre();
+	
+	/** 
+	 * Indique que le prochain niveau est la destination.
+	 */
+	void arreterProchainNiveau();
+	
+	/** 
+	 * Indique un arret.
+	 */
+	void arreter();
+	
+	
+	
 }
