1783b76253bbeaf7967c1006466bbd5131e826e2
alexisbrunet <06alexisbrunet@gmail.com>
2017-03-08 23:10:31 +0100

    ajout commentaire et fichier test case 1

--- src/iug/DoublureDeIUG.java
+++ src/iug/DoublureDeIUG.java
@@ -9,28 +9,38 @@ import controleur.IControleur;
 import commande.ListeTrieeCirculaireDeDemandes;
 
 public class DoublureDeIUG implements IIUG {
-	
-	
-	public void eteindreTousBoutons(){
-		
-	}
-	
+	/** 
+	 * Indique qu'un utilisateur souhaite se deplacer.
+	 * @param d demande.
+	 */
 	public void allumerBouton(Demande d){
-		
+		System.out.println("allumer bouton "+ d.toString());
 	}
 	
+	/** 
+	 * Indique que le palier est atteint
+	 * @param d demande.
+	 */
 	public void eteindreBouton(Demande d){
-		
+		System.out.println("eteindre bouton "+ d.toString());
 	}
 	
+	/** 
+	 * Indique un changement de position de la cabine.
+	 * @param position position de la cabine.
+	 */
 	@Override
 	public void changerPosition(int position){
-		// TODO Auto-generated method stub
+		System.out.println("franchissement de palier:  cabine en "+ position); 
 	}
 
+	/** 
+	 * ajoute un message
+	 * @param message message a ajouter.
+	 */
 	@Override
 	public void ajouterMessage(String message) {
-		// TODO Auto-generated method stub
+		System.out.println("ajout du message: "+message);
 		
 	}
 }
